211aeb206327afaf70863b9bf595419a5bc44a54
danielorta1502 <84925300+danielorta1502@users.noreply.github.com>
2021-06-24 00:01:00 -0500

    Add files via upload

---
+++ js/efectos.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+   //efecto menu
+    $('.menu a').each(function(index, elemento){
+        $(this).css ({
+            'top': '-200px'
+        });
+        $(this).animate({
+            top: '0'
+        }, 2000 +(index * 500));
+    });
+
+    //header
+    if( $(window).width() > 800){
+        $('header .textos').css({
+           opacity: 0,
+           marginTop: 0
+        });
+        $('header .textos').animate({
+            opacity: 1,
+            marginTop: '-52px'
+        },1500);
+    }
+
+
+    var acercaDE = $('#acerca-de').offset().top,
+    menu = $('#platillos').offset().top,
+    galeria = $('#galeria').offset().top,
+    ubicacion = $('#ubicacion').offset().top;
+                    
+    $('#btn-acerca-de').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 380
+        }, 500);
+    });
+
+    $('#btn-menu').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: menu
+        }, 500);
+    });
+    
+    $('#btn-galeria').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: galeria
+        }, 500);
+    });
+
+    $('#btn-ubicacion').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: ubicacion
+        }, 500);
+    });
+
+});
